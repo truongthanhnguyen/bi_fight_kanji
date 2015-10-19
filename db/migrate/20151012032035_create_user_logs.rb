65ef601c6369eb9375f0d2da3c4ad6f78a6ee327
minh93 <phamducminh.bri@gmail.com>
2015-10-19 09:27:32 +0700

    mailer confirm

---
+++ db/migrate/20151012032035_create_user_logs.rb
@@ -0,0 +1,12 @@
+class CreateUserLogs < ActiveRecord::Migration
+  def change
+    create_table :user_logs do |t|
+      t.integer :user_id
+      t.string :log_type
+      t.integer :target
+      t.string :log_data
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151005025525) do
+ActiveRecord::Schema.define(version: 20151012032035) do
+
+  create_table "user_logs", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "log_type"
+    t.integer  "target"
+    t.string   "log_data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -25,10 +34,15 @@ ActiveRecord::Schema.define(version: 20151005025525) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
