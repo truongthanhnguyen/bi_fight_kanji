111d851ada983ce990e0b8c3db9f3a0af820f7f2
MinhPD <phamducminh.bri@gmail.com>
2015-10-19 03:10:26 +0000

    finish task2_mailer

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,7 +2,7 @@ if Rails.env.development?
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
-    port: '2525'
+    port: '2525',
     authentication: :plain,
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
