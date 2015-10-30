20c5f1caafcce1be0cd9b286e90d45a9215c5d33
minh93 <phamducminh.bri@gmail.com>
2015-10-30 13:19:55 +0700

    add_user_details

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151019041410) do
+ActiveRecord::Schema.define(version: 20151026020518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,8 @@ ActiveRecord::Schema.define(version: 20151019041410) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "gender"
+    t.integer  "age"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
